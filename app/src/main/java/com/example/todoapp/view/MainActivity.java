fc4d17a03371bcdf37360c9bc65119dfc852db11
hohoanghuy13 <hohoanghuy170@gmail.com>
2024-07-22 19:00:22 +0700

    settings project

--- app/src/main/java/com/example/todoapp/view/MainActivity.java
+++ app/src/main/java/com/example/todoapp/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.todoapp;
+package com.example.todoapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.todoapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
